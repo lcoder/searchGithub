c416483618ac28d0c0687df19340bcb7fa14fd2d
毛庭峰 <mtingfeng@gmail.com>
2017-03-03 15:53:31 +0800

    add comment

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import { createStore } from 'redux' ;
 import reducer from './reducer' ;
-
+// 先看ppt的 dispatch图
 const initialState = { list: [] , isFetch: false , error: '' } ;
 
 var store = createStore( reducer , initialState ) ;
